ad2c69ed8ad562382b13cdcfdc9994f75e936d54
laserzz <82984448+laserzz@users.noreply.github.com>
2022-09-10 18:21:15 -0700

    corrected spelling mistake

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -6,7 +6,7 @@ class ErrorHandler(commands.Cog):
         self.bot = bot
 
     @commands.Cog.listener()
-    async def on_appliction_command_error(self, ctx, error):
+    async def on_application_command_error(self, ctx, error):
         embed = discord.Embed(title="Error", description=error, color=discord.Color.embed_background(theme="dark"))
         await ctx.respond(embed=embed)
 
