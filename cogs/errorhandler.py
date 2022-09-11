390e07eacd30a39889c4c7d442243d4eb0369968
Jonathan55 <102492309+Jonathan5508@users.noreply.github.com>
2022-09-10 22:24:43 -0600

    Update errorhandler.py

--- cogs/errorhandler.py
+++ cogs/errorhandler.py
@@ -7,7 +7,7 @@ class ErrorHandler(commands.Cog):
 
     @commands.Cog.listener()
     async def on_application_command_error(self, ctx, error):
-        channel = self.bot.get_channel("994731835261210634")
+        channel = self.bot.get_channel(994731835261210634)
         embed = discord.Embed(title="Error", description=f"```{error}```", color=discord.Color.embed_background(theme="dark"))
         embed2 = discord.Embed(title="Error", description=f"The developers are aware of this error. Please wait a few hours before using this command again.", color=discord.Color.embed_background(theme="dark"))
         await ctx.respond(embed=embed2)
